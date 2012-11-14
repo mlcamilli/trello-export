f3e2f96df605628148e12c08915e43cb3c2cfdeb
Matt Camilli <boos3y@ExcellaLinux.(none)>
2012-11-14 16:07:07 -0500

    basic functionality done

--- trello-export.py
+++ trello-export.py
@@ -1,7 +1,5 @@
-import os
-import sys
-import argparse
-import json
+import os, sys, argparse, json, csv
+
 
 
 def main():
@@ -9,9 +7,59 @@ def main():
 	parser.add_argument("inputfile", help="the json file containing trello board data")
 	parser.add_argument("--outputfile", help="the json file containing trello board data")
 	args = parser.parse_args()
-	print args.inputfile
+	f = open(args.inputfile)
+	json_object = json.load(f)
+	print_cards(json_object, args.outputfile)
+	
 
 
+def print_cards(json_object, outputfile):
+	
+	def checklist_data(card):
+		if len(card['idChecklists']) > 0:
+			checklist_id = card['idChecklists'][0]
+		else:
+			return ''
+		checklists = sorted(json_object['checklists'], key=lambda k: k['id'])
+		checklist = {}
+		for check in checklists:
+			if check['id'] == checklist_id:
+				checklist = check
+				break
+		string = ''
+		for item in checklist['checkItems']:
+			string += item['name'] + '\n'
+		return string[:-1] 
+
+	def labels(card):
+		label = ''
+		for lbl in card['labels']:
+			label += lbl['name'] + '\n'
+		return label[:-1] 
+	def members(card):
+		member_string = ''
+		for member in card['idMembers']:
+			for member_obj in json_object['members']:
+				if member_obj['id'] == member:
+					member_string += member_obj['fullName'] + '\n'
+					break
+		return member_string[:-1]
+
+
+
+	cards = sorted(json_object['cards'], key=lambda k: k['idShort'])
+	if outputfile != None:
+		filename = outputfile
+	else:
+		filename = 'output.csv'
+	with open(filename, 'wb') as csvfile:
+		writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		writer.writerow(['ID', 'Story', 'Description', 'Checklist', 'Labels', 'Due Date', 'Members'])
+		for card in cards:
+			writer.writerow([card['idShort'], card['name'],
+				card['desc'], checklist_data(card), labels(card), card['due'], members(card)]) 
+
+	
 
 
 
